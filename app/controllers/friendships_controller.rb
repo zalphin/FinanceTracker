b59da35c4615a47b1cd6b8706fd1a907a983824b
eric <sonic009@hotmail.com>
2020-03-03 15:19:42 +0000

    Added removing followed friends

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,11 @@
+class FriendshipsController < ApplicationController
+  def create
+  end
+
+  def destroy
+    friendship = current_user.friendships.where(friend_id: params[:id]).first
+    friendship.destroy
+    flash[:notice] = "Stopped following"
+    redirect_to friends_path
+  end
+end
