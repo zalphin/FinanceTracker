ba154110ace7b91843c51e842be09092c9196805
eric <sonic009@hotmail.com>
2020-03-03 16:58:52 +0000

    Completed course without extra features

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,11 +1,11 @@
 class Stock < ApplicationRecord
 	has_many :user_stocks
 	has_many :users, through: :user_stocks
-	
+	before_save { self.ticker = ticker.upcase }
 	validates :name, :ticker, presence: true
 	
 	def self.new_lookup(ticker_symbol)
-		
+		ticker_symbol.upcase!
 		client = IEX::Api::Client.new(
 		  publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
 		  secret_token: Rails.application.credentials.iex_client[:sandbox_api_secret],
@@ -19,6 +19,7 @@ class Stock < ApplicationRecord
 	end
 	
 	def self.check_db(ticker_symbol)
+		ticker_symbol.upcase!
 		where(ticker: ticker_symbol).first
 	end
 end
