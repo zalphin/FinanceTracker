f0b41602397a0532c3419605b52691a5fa7a3a5c
eric <sonic009@hotmail.com>
2020-03-03 15:25:06 +0000

    Added follow friends functionality

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,5 +1,12 @@
 class FriendshipsController < ApplicationController
   def create
+    current_user.friendships.build(friend_id: params[:friend])
+    if(current_user.save)
+      flash[:notice] = "Following user"
+    else
+      flash[:alert] = "Something went wrong with the tracking request"
+    end
+    redirect_to friends_path
   end
 
   def destroy
